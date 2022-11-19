79a15a01a36c3f37b493d5a2900cf23647f84bb7
Артём Ежов <dragster1001@yandex.ru>
2022-11-19 23:22:55 +0300

    Homework 1.0

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -30,34 +30,35 @@ class PostModelTest(TestCase):
         self.assertEqual(str_post, 'Тестовый пост д', 'Check post.__str__')
 
 
-def test_post_verbose_name(self):
-    """Проверяем, что verbose_name у модели Post совпадает с ожидаемым"""
-    post = PostModelTest.post
-    field_verboses = {
-        'text': 'Текст поста',
-        'pub_date': 'Дата публикации',
-        'author': 'Автор',
-        'group': 'Группа',
-    }
+    def test_post_verbose_name(self):
+        """Проверяем, что verbose_name у модели Post совпадает с ожидаемым"""
+        post = PostModelTest.post
+        field_verboses = {
+            'text': 'Текст поста',
+            'pub_date': 'Дата публикации',
+            'author': 'Автор',
+            'group': 'Группа',
+            'image': 'Картинка'
+        }
+    
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    post._meta.get_field(field).verbose_name,
+                    expected_value,
+                )
 
-    for field, expected_value in field_verboses.items():
-        with self.subTest(field=field):
-            self.assertEqual(
-                post._meta.get_field(field).verbose_name,
-                expected_value,
-            )
 
-
-def test_post_help_text(self):
-    """Проверяем, что help_text у модели Post совпадает с ожидаемым"""
-    post = PostModelTest.post
-    field_help_text = {
-        'text': 'Введите текст поста',
-        'group': 'Группа, к которой будет относиться пост',
-    }
-    for field, expected_value in field_help_text.items():
-        with self.subTest(field=field):
-            self.assertEqual(
-                post._meta.get_field(field).help_text,
-                expected_value,
-            )
+    def test_post_help_text(self):
+        """Проверяем, что help_text у модели Post совпадает с ожидаемым"""
+        post = PostModelTest.post
+        field_help_text = {
+            'text': 'Введите текст поста',
+            'group': 'Группа, к которой будет относиться пост',
+        }
+        for field, expected_value in field_help_text.items():
+            with self.subTest(field=field):
+                self.assertEqual(
+                    post._meta.get_field(field).help_text,
+                    expected_value,
+                )
